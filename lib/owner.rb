d28980515633f19beaccc7bbd7b4241e58cb5223
D1yr <alexander.m.sahli@gmail.com>
2020-08-06 14:04:23 -0400

    completed buy cat and dog

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,6 @@
+require_relative 'cat.rb'
+require_relative 'dog.rb'
+
 class Owner
   attr_reader :name, :species
   @@all = []
@@ -17,5 +20,24 @@ class Owner
   end
   def self.reset_all
     @@all = []
+  end
+  
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+  def buy_cat(cat)
+    new_cat = Cat.new(cat, self)
+    cats
+  end
+  def buy_dog(dog)
+    new_dog = Dog.new(dog, self)
+    dogs
   end
 end
